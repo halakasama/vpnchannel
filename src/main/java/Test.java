811aba4415c4e92414605455d791877f997a2192
pengfei.ren <563320321@qq.com>
2017-03-15 11:35:31 +0800

    [test] add an udp server

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,10 @@
 import org.msgpack.MessagePack;
 import org.msgpack.annotation.Message;
+import org.pcap4j.packet.IpV4Packet;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * Created by pengfei.ren on 2017/3/14.
@@ -14,15 +19,22 @@ public class Test {
     }
 
     public static void main(String[] args) throws Exception {
-        MyMessage src = new MyMessage();
-        src.name = "msgpack";
-        src.version = 0.6;
-        src.data = new byte[]{1,2,3};
-
-        MessagePack msgpack = new MessagePack();
-        // Serialize
-        byte[] bytes = msgpack.write(src);
-        // Deserialize
-        MyMessage dst = msgpack.read(bytes, MyMessage.class);
+//        MyMessage src = new MyMessage();
+//        src.name = "msgpack";
+//        src.version = 0.6;
+//        src.data = new byte[]{1,2,3};
+//
+//        MessagePack msgpack = new MessagePack();
+//        // Serialize
+//        byte[] bytes = msgpack.write(src);
+//        // Deserialize
+//        MyMessage dst = msgpack.read(bytes, MyMessage.class);
+        DatagramSocket datagramSocket = new DatagramSocket(1110, InetAddress.getByName("10.9.8.3"));
+        byte[] buf = new byte[2048];
+        DatagramPacket packet = new DatagramPacket(buf,0,buf.length);
+        while (true){
+            datagramSocket.receive(packet);
+            System.out.println(IpV4Packet.newPacket(packet.getData(),0,packet.getLength()));
+        }
     }
 }
